164626357547c4a4e6eaf58fcb1c47ff08fb89f2
eliizabethzhang <69739319+eliizabethzhang@users.noreply.github.com>
2021-03-20 18:06:24 -0400

    Update goMeet.java

--- GoMeet/src/goMeet.java
+++ GoMeet/src/goMeet.java
@@ -6,26 +6,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-//fuck fuck fuck fuck fuck fuc kuf kc ufkcu fkcu kfu fk
-public class goMeet extends Application {
-	
+
+public class goMeet extends Application implements EventHandler<ActionEvent> {
+
 	Button button;
 
 	public static void main(String[] args) {
 		launch(args);
 
 	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("goMeet");
-		button = new Button("Get Started");
-		
+		button = new Button("fuck a bear");
+
+		button.setOnAction(e -> System.out.println("bear fucked"));
+
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
-		
-		Scene scene = new Scene(layout, 900, 300);
+
+		Scene scene = new Scene(layout, 400, 300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
 }
