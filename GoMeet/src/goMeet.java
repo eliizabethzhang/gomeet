7c98c04eacbcdcb8c8177b63da33053af8a58f46
eliizabethzhang <69739319+eliizabethzhang@users.noreply.github.com>
2021-03-20 17:39:20 -0400

    make a button

---
+++ GoMeet/src/goMeet.java
@@ -0,0 +1,30 @@
+import javafx.scene.effect.Effect;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class goMeet extends Application {
+	
+	Button button;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("goMeet");
+		button = new Button("Get Started");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 900, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+}
