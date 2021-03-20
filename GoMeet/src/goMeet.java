0aba8bfa6694d80e41ab8a47b61dc569ebae10ed
eliizabethzhang <69739319+eliizabethzhang@users.noreply.github.com>
2021-03-20 18:35:47 -0400

    Update goMeet.java

--- GoMeet/src/goMeet.java
+++ GoMeet/src/goMeet.java
@@ -4,12 +4,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class goMeet extends Application implements EventHandler<ActionEvent> {
 
-	Button button;
+	Stage window;
+	Scene scene; Scene scene1; Scene scene2;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -18,17 +22,32 @@ public class goMeet extends Application implements EventHandler<ActionEvent> {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("goMeet");
-		button = new Button("fuck a bear");
-
-		button.setOnAction(e -> System.out.println("bear fucked"));
-
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-
-		Scene scene = new Scene(layout, 400, 300);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		window = primaryStage;
+		
+		Label label = new Label("lets fuck bears.");
+		Button button = new Button("fuck a bear");
+		button.setOnAction(e -> window.setScene(scene1));
+		VBox layout = new VBox(20);
+		layout.getChildren().addAll(label, button);
+		scene = new Scene(layout, 400, 300); 
+		
+		Label label1 = new Label("the bear is curious.");
+		Button button1 = new Button("fuck a bear again");
+		button1.setOnAction(e -> window.setScene(scene2));
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button1);
+		scene1 = new Scene(layout1, 200, 200);
+		
+		Label label2 = new Label("the bear liked it. fuck it again");
+		Button button2 = new Button("the bear liked it. fuck the bear again again");
+		button2.setOnAction(e -> window.setScene(scene));
+		HBox layout2 = new HBox(40);
+		layout2.getChildren().addAll(label2, button2);
+		scene2 = new Scene(layout2, 400, 200);
+		
+		window.setTitle("goMeet");
+		window.setScene(scene);
+		window.show();
 
 	}
 }
